7286522348dbe93230bbd3fdf5899d7b03f75eb0
Mathieu Jourdan <ma.jourdan@hotmail.fr>
2018-04-19 22:08:07 +0200

    Edit stationsMap.js to display all stations on the map, add different types of markers

--- js/stationsMap.js
+++ js/stationsMap.js
@@ -1,33 +1,45 @@
 // Initialize a google map in the map div
 function initMap(stationsLyon) {
-    var coordsLyon = {lat: 45.752008, lng: 4.866884};
-
+    
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: coordsLyon
+        zoom: 11.8,
+        center: {lat: 45.762324, lng: 4.852030} 
     });
     
+    var flagColor = "";
+    
     stationsLyon.forEach(function (station) {
-        //setMarker(station.position.lat, station.position.lng, station.name);
+        if (station.status === "CLOSED") {
+            flagColor = "yellow";
+        } else if (station.available_bikes > 0) {
+            flagColor = "blue";
+        } else {
+            flagColor = "red";
+        }
+        
         var marker = new google.maps.Marker({
             position: {
                 lat: station.position.lat, 
                 lng: station.position.lng
             },
-            setMap: map,
-            title: station.name
+            map: map,
+            icon: {
+                //url: "images/markers/blue_flag.png",
+                url: "images/markers/" + flagColor + "_flag.png",
+                scaledSize: new google.maps.Size(25, 25)
+            }
         });
-        
-        console.log("La station nommée " + station.name + " a bien été ajoutée à la carte aux coordonnées suivantes ... lat.: " + station.position.lat + "° N / long.: " + station.position.lng + "° E.");
     });
+    
+    //var markerCluster = new MarkerClusterer(map, marker, {
+        //imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    //});
 }
-
 
 // Récupérer les données à exploiter de toutes les stations dans un format utilisable et les stocker dans un tableau
 ajaxGet("https://cors-anywhere.herokuapp.com/https://api.jcdecaux.com/vls/v1/stations?contract=lyon&apiKey=699ee067b85c71a4f7ca9adfdcaaa5145b06a437", function (reponse) {
     // Récupération des données dans un tableau Javascript
     var stationsLyon = JSON.parse(reponse);
-    
     initMap(stationsLyon);
 });
 
