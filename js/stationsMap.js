99050ea686631970e220b3aeac726d4cdb1b6888
Mathieu Jourdan <ma.jourdan@hotmail.fr>
2018-04-21 02:23:36 +0200

    add a marker cluster feature to the map

--- js/stationsMap.js
+++ js/stationsMap.js
@@ -1,14 +1,9 @@
-// Initialize a google map in the map div
-function initMap(stationsLyon) {
-    
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 11.8,
-        center: {lat: 45.762324, lng: 4.852030} 
-    });
-    
-    var flagColor = "";
-    
+// Create a filtered tab from the JC Decaux data tab
+function stationsTabBuilder(stationsLyon) {
+    var stationsTab = [];
     stationsLyon.forEach(function (station) {
+        
+        var flagColor = "";
         if (station.status === "CLOSED") {
             flagColor = "yellow";
         } else if (station.available_bikes > 0) {
@@ -17,30 +12,63 @@ function initMap(stationsLyon) {
             flagColor = "red";
         }
         
-        var marker = new google.maps.Marker({
+        var newStation = {
+            number: station.number,
+            name: station.name,
+            address: station.address,
+            latitude: station.position.lat,
+            longitude: station.position.lng,
+            status: station.status,
+            bike_stands: station.bike_stands,
+            available_bikes: station.available_bikes,
             position: {
-                lat: station.position.lat, 
+                lat: station.position.lat,
                 lng: station.position.lng
             },
-            map: map,
-            icon: {
-                //url: "images/markers/blue_flag.png",
-                url: "images/markers/" + flagColor + "_flag.png",
-                scaledSize: new google.maps.Size(25, 25)
-            }
-        });
+            flag_color: flagColor
+        }
+        stationsTab.push(newStation);
+    });
+    return stationsTab;
+}
+
+// Display a google map in the "map" div
+function initMap(stationsLyon) {
+    
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11.8,
+        center: {
+            lat: 45.754424, 
+            lng: 4.858030
+        },
+        fullscreenControl: false,
+        mapTypeControl: false,
+        streetViewControl: false
     });
     
-    //var markerCluster = new MarkerClusterer(map, marker, {
-        //imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-    //});
+    // Create a tab of marker objects to be displayed as elements of the marker cluster
+    var markers = [];
+    stationsLyon.forEach(function (station) {
+        var marker = new google.maps.Marker({
+            position: station.position,
+            map: map,
+            icon: {
+                url: "images/markers/" + station.flag_color + "_flag.png",
+                scaledSize: new google.maps.Size(40, 40)
+            }
+        });
+        markers.push(marker);
+    });
+    
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
 }
 
 // Récupérer les données à exploiter de toutes les stations dans un format utilisable et les stocker dans un tableau
 ajaxGet("https://cors-anywhere.herokuapp.com/https://api.jcdecaux.com/vls/v1/stations?contract=lyon&apiKey=699ee067b85c71a4f7ca9adfdcaaa5145b06a437", function (reponse) {
     // Récupération des données dans un tableau Javascript
     var stationsLyon = JSON.parse(reponse);
+    stationsLyon = stationsTabBuilder(stationsLyon);
     initMap(stationsLyon);
 });
-
-// Générer un marqueur dépendant du fait qu'il soit possible de réserver un vélo ou non (dépendant lui-même du status de la station et du nombre de vélo dispo)
