f88f818d5a074070c0a0dd37324bf7a689344309
Mathieu Jourdan <ma.jourdan@hotmail.fr>
2018-05-03 07:11:13 +0200

    Edit Booking class that should have been uploaded in the previous commit

--- js/Booking.model.js
+++ js/Booking.model.js
@@ -1,5 +1,4 @@
 class Booking {
-    //constructor(name, address, latitude, longitude, status, bike_stands, available_bikes) {
     constructor(name, bike_stands, available_bikes) {
         this.station = name;
         this.bike_stands = bike_stands;
